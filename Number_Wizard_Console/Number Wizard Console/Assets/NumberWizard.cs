ead1ab5105e0e9fe41f720f6f9b5fa2885e4d8b0
Alex Brawn <sasha@brawn.org>
2018-10-15 12:41:55 -0500

    Begin Number Wizard

---
+++ Number_Wizard_Console/Number Wizard Console/Assets/NumberWizard.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NumberWizard : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        int max = 1000;
+        int min = 1;
+
+        Debug.Log("The Number Wizard welcomes you to his tower");
+        Debug.Log("He instructs you to think of a number");
+        Debug.Log("The largest number you may pick is: " + max);
+        Debug.Log("The smallest number you may pick is: " + min);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
